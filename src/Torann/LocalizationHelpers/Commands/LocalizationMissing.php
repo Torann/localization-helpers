88b266b673fbd28c3227746e8006a6608a3417a0
Daniel Stainback <torann@gmail.com>
2016-09-25 14:04:04 -0400

    Cleanup and remove &period; from future versions.

--- src/Torann/LocalizationHelpers/Commands/LocalizationMissing.php
+++ src/Torann/LocalizationHelpers/Commands/LocalizationMissing.php
@@ -27,17 +27,13 @@ class LocalizationMissing extends LocalizationAbstract
      */
     public function fire()
     {
-        define('SUCCESS', 0);
-        define('ERROR', 1);
-
         $folders = $this->getPath($this->folders);
         $this->display = !$this->option('silent');
 
         //////////////////////////////////////////////////
         // Display where translations are searched in //
         //////////////////////////////////////////////////
-        if ($this->option('verbose'))
-        {
+        if ($this->option('verbose')) {
             $this->line("Lemmas will be searched in the following directories:");
 
             foreach ($folders as $path) {
@@ -52,13 +48,10 @@ class LocalizationMissing extends LocalizationAbstract
         ////////////////////////////////
         $lemmas = [];
 
-        foreach ($folders as $path)
-        {
-            foreach ($this->getPhpFiles($path) as $php_file_path => $dumb)
-            {
+        foreach ($folders as $path) {
+            foreach ($this->getPhpFiles($path) as $php_file_path => $dumb) {
                 $lemma = [];
-                foreach ($this->extractTranslationFromFile($php_file_path) as $k => $v)
-                {
+                foreach ($this->extractTranslationFromFile($php_file_path) as $k => $v) {
                     $real_value = eval("return $k;");
                     $lemma[$real_value] = $php_file_path;
                 }
@@ -67,8 +60,7 @@ class LocalizationMissing extends LocalizationAbstract
             }
         }
 
-        if (count($lemmas) === 0)
-        {
+        if (count($lemmas) === 0) {
             $this->comment("No lemma have been found in code.");
             $this->line("I have searched recursively in PHP files in these directories:");
 
@@ -87,10 +79,8 @@ class LocalizationMissing extends LocalizationAbstract
 
         $this->line((count($lemmas) > 1) ? count($lemmas) . " lemmas have been found in code" : "1 lemma has been found in code");
 
-        if ($this->option('verbose'))
-        {
-            foreach ($lemmas as $key => $value)
-            {
+        if ($this->option('verbose')) {
+            foreach ($lemmas as $key => $value) {
                 if (strpos($key, '.') !== false) {
                     $this->line('    <info>' . $key . '</info> in file <comment>' . $this->getShortPath($value) . '</comment>');
                 }
@@ -102,8 +92,7 @@ class LocalizationMissing extends LocalizationAbstract
         /////////////////////////////////////////////
         $lemmas_structured = [];
 
-        foreach ($lemmas as $key => $value)
-        {
+        foreach ($lemmas as $key => $value) {
             if (strpos($key, '.') === false) {
                 $this->line('    <error>' . $key . '</error> in file <comment>' . $this->getShortPath($value) . '</comment> <error>will not be included because it has no parent</error>');
             }
@@ -111,7 +100,7 @@ class LocalizationMissing extends LocalizationAbstract
                 array_set(
                     $lemmas_structured,
                     $key,
-                    str_replace('&period;', '.', $value)
+                    str_replace('&period;', '.', $value) // @deprecated in version 1.5
                 );
             }
         }
@@ -129,16 +118,11 @@ class LocalizationMissing extends LocalizationAbstract
         $there_are_new = false;
 
         $this->line('Scan files:');
-        foreach (scandir($dir_lang) as $lang)
-        {
-            if (!in_array($lang, [".", ".."]))
-            {
-                if (is_dir($dir_lang . DIRECTORY_SEPARATOR . $lang))
-                {
-                    foreach ($lemmas_structured as $family => $array)
-                    {
-                        if (in_array($family, $this->ignore_lang_files))
-                        {
+        foreach (scandir($dir_lang) as $lang) {
+            if (!in_array($lang, [".", ".."])) {
+                if (is_dir($dir_lang . DIRECTORY_SEPARATOR . $lang)) {
+                    foreach ($lemmas_structured as $family => $array) {
+                        if (in_array($family, $this->ignore_lang_files)) {
                             if ($this->option('verbose')) {
                                 $this->line('');
                                 $this->info("\t! Skip lang file '{$family}' !");
@@ -198,57 +182,58 @@ class LocalizationMissing extends LocalizationAbstract
                         //////////////////////////
                         // Deal with new lemmas //
                         //////////////////////////
-                        if (count($welcome_lemmas) > 0)
-                        {
+                        if (count($welcome_lemmas) > 0) {
                             $display_already_comment = true;
                             $something_to_do = true;
                             $there_are_new = true;
                             $this->info("\t" . count($welcome_lemmas) . " new strings to translate");
                             $final_lemmas["TRANS___NEW___TRANS"] = "TRANS___NEW___TRANS";
 
-                            foreach ($welcome_lemmas as $key => $path)
-                            {
+                            foreach ($welcome_lemmas as $key => $path) {
                                 $value = $key;
 
-                                // Clean up the key value
-                                if (substr($key, 0, strpos($key, '.'))) {
-                                    $value = substr($key, strpos($key, '.') + 1);
-                                }
+//                                // Clean up the key value
+//                                if (substr($key, 0, strpos($key, '.'))) {
+//                                    $value = substr($key, strpos($key, '.') + 1);
+//                                }
 
                                 if ($this->option('verbose')) {
                                     $this->line("\t\t<info>{$key}</info> in " . $this->getShortPath($path));
                                 }
 
-                                if (! $this->option('no-comment')) {
+                                if (!$this->option('no-comment')) {
                                     $final_lemmas['TRANS___COMMENT___TRANS' . $i] = "Defined in file $path";
                                     $i = $i + 1;
                                 }
 
-                                array_set($final_lemmas,
-                                    $key,
-                                    str_replace('%LEMMA', str_replace('&period;', '.', $value),
-                                        $this->option('new-value'))
-                                );
+                                // Clean up sentence period punctuation
+                                if ($this->isMultidimensional($key)) {
+                                    array_set($final_lemmas,
+                                        $key,
+                                        str_replace('%LEMMA', $value, $this->option('new-value'))
+                                    );
+                                }
+                                else {
+                                    $final_lemmas[$key] = str_replace('%LEMMA', $value, $this->option('new-value'));
+                                }
                             }
                         }
 
                         ///////////////////////////////
                         // Deal with existing lemmas //
                         ///////////////////////////////
-                        if (count($already_lemmas) > 0)
-                        {
+                        if (count($already_lemmas) > 0) {
                             if ($this->option('verbose')) {
                                 $this->line("\t\t\t" . count($already_lemmas) . " already translated strings");
                             }
 
                             $final_lemmas["TRANS___OLD___TRANS"] = "TRANS___OLD___TRANS";
 
-                            foreach ($already_lemmas as $key => $value)
-                            {
+                            foreach ($already_lemmas as $key => $value) {
                                 array_set(
                                     $final_lemmas,
                                     $key,
-                                    str_replace('&period;', '.', $value)
+                                    str_replace('&period;', '.', $value) // @deprecated in version 1.5
                                 );
                             }
                         }
@@ -256,13 +241,10 @@ class LocalizationMissing extends LocalizationAbstract
                         ///////////////////////////////
                         // Deal with obsolete lemmas //
                         ///////////////////////////////
-                        if (count($obsolete_lemmas) > 0)
-                        {
+                        if (count($obsolete_lemmas) > 0) {
                             // Remove all dynamic fields
-                            foreach ($obsolete_lemmas as $key => $value)
-                            {
-                                foreach ($this->never_obsolete_keys as $remove)
-                                {
+                            foreach ($obsolete_lemmas as $key => $value) {
+                                foreach ($this->never_obsolete_keys as $remove) {
                                     $remove = "{$remove}.";
 
                                     if (substr($key, 0, strlen($remove)) === $remove
@@ -271,7 +253,7 @@ class LocalizationMissing extends LocalizationAbstract
 
                                         array_set($final_lemmas,
                                             $key,
-                                            str_replace('%LEMMA', str_replace('&period;', '.', $value),
+                                            str_replace('%LEMMA', str_replace('&period;', '.', $value), // @deprecated in version 1.5
                                                 $this->option('new-value'))
                                         );
 
@@ -281,8 +263,7 @@ class LocalizationMissing extends LocalizationAbstract
                             }
                         }
 
-                        if (count($obsolete_lemmas) > 0)
-                        {
+                        if (count($obsolete_lemmas) > 0) {
                             $display_already_comment = true;
                             $something_to_do = true;
 
@@ -293,8 +274,7 @@ class LocalizationMissing extends LocalizationAbstract
 
                             $final_lemmas["TRANS___OBSOLETE___TRANS"] = "TRANS___OBSOLETE___TRANS";
 
-                            foreach ($obsolete_lemmas as $key => $value)
-                            {
+                            foreach ($obsolete_lemmas as $key => $value) {
                                 if ($this->option('verbose')) {
                                     $this->line("\t\t\t<comment>{$key}</comment>");
                                 }
@@ -303,14 +283,13 @@ class LocalizationMissing extends LocalizationAbstract
                                     array_set(
                                         $final_lemmas,
                                         $key,
-                                        str_replace('&period;', '.', $value)
+                                        str_replace('&period;', '.', $value) // @deprecated in version 1.5
                                     );
                                 }
                             }
                         }
 
-                        if (($something_to_do === true) || ($this->option('force')))
-                        {
+                        if (($something_to_do === true) || ($this->option('force'))) {
                             $content = var_export($final_lemmas, true);
                             $content = preg_replace("@'TRANS___COMMENT___TRANS[0-9]*' => '(.*)',@", '// $1', $content);
                             $content = str_replace(
@@ -344,21 +323,19 @@ class LocalizationMissing extends LocalizationAbstract
         // Silent mode                           //
         // only return an exit code on new lemma //
         ///////////////////////////////////////////
-        if ($this->option('silent'))
-        {
+        if ($this->option('silent')) {
             if ($there_are_new === true) {
-                return ERROR;
+                return false;
             }
             else {
-                return SUCCESS;
+                return true;
             }
         }
 
         ///////////////////////////////////////////
         // Normal mode                           //
         ///////////////////////////////////////////
-        if (count($job) > 0)
-        {
+        if (count($job) > 0) {
             if ($this->option('no-interaction')) {
                 $do = true;
             }
@@ -368,13 +345,11 @@ class LocalizationMissing extends LocalizationAbstract
                 $this->line('');
             }
 
-            if ($do === true)
-            {
+            if ($do === true) {
                 if (!$this->option('no-backup')) {
                     $this->line('Backup files:');
 
-                    foreach ($job as $file_lang_path => $file_content)
-                    {
+                    foreach ($job as $file_lang_path => $file_content) {
                         $backup_path = preg_replace('/\..+$/', '.' . date("Ymd_His") . '.php', $file_lang_path);
 
                         if (!$this->option('dry-run')) {
@@ -389,8 +364,7 @@ class LocalizationMissing extends LocalizationAbstract
 
                 $this->line('Save files:');
                 $open_files = '';
-                foreach ($job as $file_lang_path => $file_content)
-                {
+                foreach ($job as $file_lang_path => $file_content) {
                     if (!$this->option('dry-run')) {
                         file_put_contents($file_lang_path, $file_content);
                     }
@@ -417,7 +391,7 @@ class LocalizationMissing extends LocalizationAbstract
         }
         else {
             if ($this->option('silent')) {
-                return SUCCESS;
+                return true;
             }
 
             $this->line('');
@@ -425,6 +399,19 @@ class LocalizationMissing extends LocalizationAbstract
         }
 
         $this->line('');
+    }
+
+    /**
+     * Determine if the given string is ment to be a
+     * multidimensional array.
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+    protected function isMultidimensional($string)
+    {
+        return str_contains(preg_replace('/(\.\s|\.$)/', '&period;', $string), '.');
     }
 
     /**
@@ -453,7 +440,7 @@ class LocalizationMissing extends LocalizationAbstract
                 'l',
                 InputOption::VALUE_OPTIONAL,
                 'Value of new found lemmas (use %LEMMA for the lemma value)',
-                '%LEMMA'
+                '%LEMMA',
             ],
             ['no-backup', 'b', InputOption::VALUE_NONE, 'Do not backup lang file (be careful, I am not a good coder)'],
             ['no-comment', 'c', InputOption::VALUE_NONE, 'Do not add comments in lang files for lemma definition'],
@@ -462,7 +449,7 @@ class LocalizationMissing extends LocalizationAbstract
                 'silent',
                 's',
                 InputOption::VALUE_NONE,
-                'Use this option to only return the exit code (use $? in shell to know whether there are missing lemma)'
+                'Use this option to only return the exit code (use $? in shell to know whether there are missing lemma)',
             ],
         ];
     }
