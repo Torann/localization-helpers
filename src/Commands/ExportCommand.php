3e0c6039f84e8a1e772fd2b4d477cfd4f1f03a87
Daniel Stainback <torann@gmail.com>
2017-10-06 10:27:19 -0400

    Add handle method in order to delegate to fire method for Laravel 5.5

--- src/Commands/ExportCommand.php
+++ src/Commands/ExportCommand.php
@@ -39,11 +39,21 @@ class ExportCommand extends AbstractCommand
     }
 
     /**
-     * Execute the console command.
+     * Execute the console command for Laravel 5.4 and below
      *
-     * @return mixed
+     * @return void
      */
     public function fire()
+    {    
+        $this->handle();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
     {
         $locale = $this->argument('locale');
         $group = $this->argument('group');
